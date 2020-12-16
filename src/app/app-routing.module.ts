797789d1645648e7589e51fe07184dff5ad9bc24
samsonemeje <samsonemeje@gmail.com>
2020-12-16 18:49:52 +0100

    start frame for recommend screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'recommend',
+    loadChildren: () => import('./recommend/recommend.module').then( m => m.RecommendPageModule)
+  },
 ];
 
 @NgModule({
